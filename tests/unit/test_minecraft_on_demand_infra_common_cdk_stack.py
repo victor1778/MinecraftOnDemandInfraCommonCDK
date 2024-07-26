b96b6a9776001153848a1b7b677516c4949c21d1
Victor Fonseca Vega <vrfvega@gmail.com>
2024-07-26 16:19:57 -0400

    Initial commit

---
+++ tests/unit/test_minecraft_on_demand_infra_common_cdk_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from minecraft_on_demand_infra_common_cdk.minecraft_on_demand_infra_common_cdk_stack import MinecraftOnDemandInfraCommonCdkStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in minecraft_on_demand_infra_common_cdk/minecraft_on_demand_infra_common_cdk_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = MinecraftOnDemandInfraCommonCdkStack(app, "minecraft-on-demand-infra-common-cdk")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
